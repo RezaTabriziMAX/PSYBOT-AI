0d7ad2023d6c37b1004aa51547cb3af19bee0a40
Nuttoo <jfj22342@gmail.com>
2025-12-20 14:01:09 +0800

    Create http.ts

---
+++ apps-api/src/types/http.ts
@@ -0,0 +1,13 @@
+export type ApiErrorBody = {
+  error: {
+    code: string;
+    message: string;
+    requestId?: string;
+    details?: unknown;
+  };
+};
+
+export type Pagination = {
+  take?: number;
+  cursor?: string;
+};
