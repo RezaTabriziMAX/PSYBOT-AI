7dadccc4593e7eab04ea73b94147258aa2a2bdbd
Nuttoo <jfj22342@gmail.com>
2025-12-20 12:50:41 +0800

    Create anchor.ts

---
+++ packages/solana/src/types/anchor.ts
@@ -0,0 +1,10 @@
+export type AnchorIdl = {
+  version: string;
+  name: string;
+  instructions: any[];
+  accounts?: any[];
+  types?: any[];
+  events?: any[];
+  errors?: any[];
+  metadata?: any;
+};
