09efc80c626123e75dcda0424b0c56a7fb2c0347
Nuttoo <jfj22342@gmail.com>
2025-12-20 15:09:32 +0800

    Create mod.rs

---
+++ programs/nuttoo_registry/src/instructions/mod.rs
@@ -0,0 +1,15 @@
+pub mod init_registry;
+pub mod publish_module;
+pub mod update_module;
+pub mod create_fork;
+pub mod attest_artifact;
+pub mod record_run;
+pub mod set_policy;
+
+pub use init_registry::*;
+pub use publish_module::*;
+pub use update_module::*;
+pub use create_fork::*;
+pub use attest_artifact::*;
+pub use record_run::*;
+pub use set_policy::*;
