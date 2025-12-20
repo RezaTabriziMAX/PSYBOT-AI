e07c149fd904f11bcf887c44a3431c5c073962e5
Nuttoo <jfj22342@gmail.com>
2025-12-20 11:42:59 +0800

    Create variables.tf

---
+++ infra/terraform/modules/vpc/variables.tf
@@ -0,0 +1,16 @@
+variable "cidr_block" {
+  type = string
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
